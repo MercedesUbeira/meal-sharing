f288adf9a0152a6fb5cd2389b0eaa9cfcc31d013
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-05-17 13:33:52 +0200

    Created MealsList component. (Doesn't work yet)

--- src/client/App.js
+++ src/client/App.js
@@ -1,21 +1,50 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import TestComponent from "./components/TestComponent/TestComponent";
+import React from 'react';
+import './App.css';
+import MealsList from './MealsList';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import MealsList from "./components/MealsList";
+// import TestComponent from "./components/TestComponent/TestComponent";
+// import { response } from "../backend/app";
+
 
 function App() {
   return (
     <Router>
-      <Route exact path="/">
-        <p>test</p>
-      </Route>
-      <Route exact path="/lol">
-        <p>lol</p>
-      </Route>
-      <Route exact path="/test-component">
-        <TestComponent></TestComponent>
-      </Route>
-    </Router>
+      <div className='App'>
+        <Routes>
+          <Route path="/whatsUpp" element ={<MealsList />} />
+        </Routes>
+      </div>
+      </Router>
   );
 }
+
+// function App() {
+//   return (
+//     <Router>
+//       <Switch>
+//       <Route exact path="/">
+//         <p>adsasdas</p>
+//       </Route>
+//       <Route exact path="/lol">
+//         <p>hELLO LUCAS</p>
+//       </Route>
+//       <Route exact path="/test-component">
+//         <TestComponent></TestComponent>
+//       </Route>
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+// function App() {
+//     return (
+//       <div className="App">
+//         <h1>DAasdasdas</h1>
+//         <MealsList />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/client/components/TestComponent/TestComponent.js
+++ src/client/components/TestComponent/TestComponent.js
@@ -1,13 +1,13 @@
 /* eslint-disable camelcase */
 /* eslint-disable @typescript-eslint/camelcase */
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import "./testComponentStyle.css";
 
 export default function TestComponent() {
   return (
     <section className="test-component">
-      <p>in a component</p>
+      <p>PLEASE CHANGEEEE</p>
     </section>
   );
 }
